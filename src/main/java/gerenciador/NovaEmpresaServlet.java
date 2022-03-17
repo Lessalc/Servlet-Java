8cd243388d440d3f1fd6005da96e44bfa146c2f3
Lessalc <lucianoclessa@gmail.com>
2022-03-16 22:31:16 -0300

    Trabalhando com Parâmetros em nossa HTTP
    Primeira classe servlet mapeada para chamar código em HTML
    Criado um formulário em HTML para que fosse passado via HTTP uma
    requisição na URL para ser consumida por uma classe servlet.

---
+++ src/main/java/gerenciador/NovaEmpresaServlet.java
@@ -0,0 +1,29 @@
+package gerenciador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NovaEmpresaServlet
+ */
+@WebServlet("/novaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Cadastrando nova empresa");
+		String name = request.getParameter("nome");
+  		PrintWriter out = response.getWriter();
+  		out.println("<html><body><h3>Empresa "+ name +" Cadastrada!</h3></body></html>");
+	}
+
+}
